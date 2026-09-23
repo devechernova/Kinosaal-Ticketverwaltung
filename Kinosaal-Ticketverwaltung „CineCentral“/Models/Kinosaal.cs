292c898fbc35cd01ef5e65402b5361348a747d7a
Ekaterina Chernova <devechernova@gmail.com>
2026-09-23 16:06:56 +0200

    Create project structure and add Kinosaal model

---
+++ Kinosaal-Ticketverwaltung „CineCentral“/Models/Kinosaal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinosaal_Ticketverwaltung__CineCentral_.Models
+{
+    internal class Kinosaal
+    {
+    }
+}
---
+++ Kinosaal-Ticketverwaltung „CineCentral“/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kinosaal_Ticketverwaltung__CineCentral_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
