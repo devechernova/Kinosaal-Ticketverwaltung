e68cecb103120217f07d3c2f450c2125fb02c55e
Ekaterina Chernova <devechernova@gmail.com>
2026-09-24 08:27:08 +0200

    Implement Kinosaal constructor and prepare seat initialization

--- Kinosaal-Ticketverwaltung „CineCentral“/Models/Kinosaal.cs
+++ Kinosaal-Ticketverwaltung „CineCentral“/Models/Kinosaal.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Kinosaal_Ticketverwaltung__CineCentral_.Models
 {
-    internal class Kinosaal
+    public class Kinosaal
     {
+        public int AnzahlReihen { get; set; }
+        public int AnzahlSitze {  get; set; }
+
+        public char[,] Sitzplaetze { get; set; }
+
+        public Kinosaal(int anzahlReihen, int anzahlSitze)
+        {
+        AnzahlReihen = anzahlReihen;
+        AnzahlSitze = anzahlSitze;
+
+            Sitzplaetze = new char[AnzahlReihen, AnzahlSitze];
+        }
+       
     }
 }
--- Kinosaal-Ticketverwaltung „CineCentral“/Program.cs
+++ Kinosaal-Ticketverwaltung „CineCentral“/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace Kinosaal_Ticketverwaltung__CineCentral_
+﻿namespace Kinosaal_Ticketverwaltung__CineCentral_;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
