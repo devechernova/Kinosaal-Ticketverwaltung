7a9a97c111e05c4ebf579d49af85bb8164989342
Ekaterina Chernova <devechernova@gmail.com>
2026-09-24 10:47:26 +0200

    Connect menu with seat management service

--- Kinosaal-Ticketverwaltung „CineCentral“/Program.cs
+++ Kinosaal-Ticketverwaltung „CineCentral“/Program.cs
@@ -9,7 +9,42 @@ public class Program
     {
        Kinosaal kinosaal = new Kinosaal(10, 5);
        AnzeigeService anzeigeService = new AnzeigeService();
+       KinosaalService kinosaalService = new KinosaalService();
 
-       anzeigeService.ZeigeKinosaal(kinosaal);
+        anzeigeService.ZeigeKinosaal(kinosaal);
+       
+        Console.WriteLine();
+        Console.WriteLine("Aktion wählen:");
+        Console.WriteLine("b = belegen");
+        Console.WriteLine("r = reservieren");
+        Console.WriteLine("f = freigeben");
+
+        string aktion = Console.ReadLine();
+
+        Console.Write("Reihe: ");
+        int reihe = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Sitz: ");
+        int sitz = Convert.ToInt32(Console.ReadLine());
+
+        if (aktion == "b")
+        {
+            kinosaalService.BelegeSitz(kinosaal, reihe, sitz);
+        }
+
+        if (aktion == "r")
+        {
+            kinosaalService.ReserviereSitz(kinosaal, reihe, sitz);
+        }
+
+        if (aktion == "f")
+        {
+            kinosaalService.GibSitzFrei(kinosaal, reihe, sitz);
+        }
+
+        anzeigeService.ZeigeKinosaal(kinosaal);
+        Console.WriteLine();
+        Console.WriteLine("Beliebige Taste drücken...");
+        Console.ReadKey();
     }
 }
