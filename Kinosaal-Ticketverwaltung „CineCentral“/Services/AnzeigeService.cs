4f5c0aca388bf19fb7373eda2cfc9a1a969c086b
Ekaterina Chernova <devechernova@gmail.com>
2026-09-24 08:58:06 +0200

    Add AnzeigeService for cinema hall display

---
+++ Kinosaal-Ticketverwaltung „CineCentral“/Services/AnzeigeService.cs
@@ -0,0 +1,16 @@
+﻿using Kinosaal_Ticketverwaltung__CineCentral_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinosaal_Ticketverwaltung__CineCentral_.Services;
+
+public class AnzeigeService
+{
+    public void ZeigeKinosaal(Kinosaal kinosaal)
+    {
+
+    }
+}
